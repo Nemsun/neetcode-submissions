fdd57e8d0b99c36eb8fcc1b0269e5639720969f1
Nemsun <98297766+Nemsun@users.noreply.github.com>
2026-06-12 20:35:46 -0500

    Remove: duplicate-integer - submission-1

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        # use a set
-        # time complexity O(1)
-        # space complexity O(n)
-
-        dupe_set = set(nums)
-
-        return len(dupe_set) != len(nums)
