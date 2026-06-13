bb9d15989db3760e970ad370592b244730dc518b
Nemsun <98297766+Nemsun@users.noreply.github.com>
2026-06-12 20:35:47 -0500

    Remove: duplicate-integer - submission-2

--- Data Structures & Algorithms/duplicate-integer/submission-2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        num_map = {}
-
-        for i in nums:
-            num_map[i] = num_map.get(i, 0) + 1
-            if num_map[i] > 1:
-                return True
-        return False
